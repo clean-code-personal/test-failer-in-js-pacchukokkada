572f89a0b24a15f41e220f48f6b9c7d65d2f917f
pacchukokkada <pacchukok@gmail.com>
2023-03-08 16:43:30 +0530

    added strong tests

--- misaligned.js
+++ misaligned.js
@@ -3,9 +3,10 @@ const { test } = require('./test/misalignedTest');
 
 let manual = [];
 let colors = [];
+const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
+const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
 function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+    
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
             console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
@@ -13,8 +14,9 @@ function print_color_map() {
             manual.push(st);
             let col = [];
             col.push(majorColors[i]);
-            col.push(minorColors[i]);
+            col.push(minorColors[j]);
             colors.push(col); 
+
         }
     }
     return majorColors.length * minorColors.length;
@@ -26,7 +28,9 @@ function print_color_map() {
 
 module.exports = {
     manual,
-    colors
+    colors,
+    majorColors,
+    minorColors
 }
 let size = print_color_map();
 test()
--- test/misalignedTest.js
+++ test/misalignedTest.js
@@ -2,23 +2,39 @@
 
 //function to check the alignment
 function checkAlignment(manual){
-    if(manual[0][3] != '|' || manual[10][3] != '|')
-        return 0;
-    if(manual[0][12] != '|' || manual[5][12] != '|' || manual[10][12] != '|' || manual[15][12] != '|')
-        return 0;
+    for(let i=0;i<manual.length;i++)
+        if(manual[i][3] != '|' || manual[i][12] != '|')
+            return 0;
+    return 1;
+}
+
+//function to check the color combination
+function checkColorCombination(colors,majorColors,minorColors)
+{
+    let m=0;
+    for(let i=0;i<5;i++)
+    {
+        for(let j=0;j<5;j++)
+        {
+            if(colors[m][0] != majorColors[i] || colors[m][1] != minorColors[j])
+                return 0;
+            m = m+1;
+        }
+    }
     return 1;
 }
 
 function test(){
-    const { manual,colors } = require('../misaligned');
+    const { manual,colors, majorColors, minorColors } = require('../misaligned');
     const { expect } = require('chai');
     //checking whether code starts from 1 or not
     expect(manual[0][0]).equal('1');
-    //checking whether the color combination is correct or not 
+    // //checking whether the color combination is correct or not 
     expect(colors[0][0]).equal('White');
     expect(colors[0][1]).equal('Blue');
-    //checking the alignment of the separator
+    // //checking the alignment of the separator
     expect(checkAlignment(manual)).equal(1);
+    expect(checkColorCombination(colors,majorColors,minorColors)).equal(1);
     console.log("All is well");
 }
 
