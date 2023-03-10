f0ad0b19c01d7ee570976a58490e825224fc4426
Prashanth <61051191+pacchukokkada@users.noreply.github.com>
2023-03-10 12:56:54 +0530

    updated implementation

--- alerter.js
+++ alerter.js
@@ -1,24 +1,21 @@
 const { networkAlertStub } = require("./networkStub");
-const { test } = require('./test/alerterTest');
+
 let alertFailureCount = 0;
 function alertInCelcius(farenheit) {
     const celcius = (farenheit - 32) * 5 / 9;
     const returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
-module.exports = {
-    alertFailureCount,
+//function to return alertFailure count
+function getAlertFailureCount(){
+    return alertFailureCount;
 }
 
-//testing
-alertInCelcius(400.5);
-alertInCelcius(303.6);
-test();
+module.exports = {
+    getAlertFailureCount,
+    alertInCelcius
+}
 
